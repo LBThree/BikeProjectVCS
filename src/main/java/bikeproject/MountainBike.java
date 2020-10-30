142fce4f414e99be753a3f8cddb0c38859fe7ca0
LBThree <loganbahr94@gmail.com>
2020-10-30 19:13:15 -0400

    2-3 Activity Completed + Made Bike an abstract class so that objects cannot be made from it + Created toString methods for RoadBike, Bike, and MountainBike - Removed printDescription for Roadbike, Bike, and MountainBike - removed object bike4 because it was of type Bike and could not longer be instantiated

--- src/main/java/bikeproject/MountainBike.java
+++ src/main/java/bikeproject/MountainBike.java
@@ -1,47 +1,46 @@
 package bikeproject;
 
-public class MountainBike extends Bike implements MountainParts{
+public class MountainBike extends Bike implements MountainParts {
 
-	private String suspension, type;
-	private int frameSize;
-	
-	public MountainBike()
-	{
-		this("Bull Horn", "Hardtail", "Maxxis", "dropper", 27, "RockShox XC32", "Pro", 19);
-	}//end constructor
-	
-    public MountainBike(String handleBars, String frame, String tyres, String seatType, int numGears, 
-    		            String suspension, String type, int frameSize) {
-		super(handleBars, frame, tyres, seatType, numGears);
-		this.suspension = suspension;
-		this.type = type;
-		this.frameSize = frameSize;
-	}//end constructor
-    
-	public void printDescription()
-	{
-		super.printDescription();
-		System.out.println("This mountain bike is a " + this.type + " bike and has a " + this.suspension + " suspension and a frame size of " + this.frameSize + "inches.");
-		
-	}//end method printDescription
+  private String suspension, type;
+  private int frameSize;
 
-	@Override
-	public String getSuspension() {
-		return suspension;
-	}
+  public MountainBike() {
+    this("Bull Horn", "Hardtail", "Maxxis", "dropper", 27, "RockShox XC32", "Pro", 19);
+  }//end constructor
 
-	@Override
-	public void setSuspension(String newValue) {
+  public MountainBike(String handleBars, String frame, String tyres, String seatType, int numGears,
+      String suspension, String type, int frameSize) {
+    super(handleBars, frame, tyres, seatType, numGears);
+    this.suspension = suspension;
+    this.type = type;
+    this.frameSize = frameSize;
+  }//end constructor
 
-	}
 
-	@Override
-	public String getType() {
-		return type;
-	}
+  @Override
+  public String toString() {
+    return super.toString() + "This mountain bike is a " + this.type + " bike and has a "
+        + this.suspension + " suspension and a frame size of " + this.frameSize + "inches.";
+  }//end method toString
 
-	@Override
-	public void setType(String newValue) {
+  @Override
+  public String getSuspension() {
+    return suspension;
+  }
 
-	}
+  @Override
+  public void setSuspension(String newValue) {
+
+  }
+
+  @Override
+  public String getType() {
+    return type;
+  }
+
+  @Override
+  public void setType(String newValue) {
+
+  }
 }//end class MountainBike
